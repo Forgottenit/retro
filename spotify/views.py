565342034f4bd637323e8a2c0d4b92b71fda1670
Forgottenit <forgottenit@hotmail.com>
2023-06-12 18:41:20 +0000

    Changed views.py in spotify app

--- spotify/views.py
+++ spotify/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
-from django.shortcuts import render
 import json
-
 
 def album_view(request):
     # Load data from the JSON file
     with open('album_fixtures.json', 'r') as file:
         data = json.load(file)
 
-    # Extract the relevant fields from the data
-    album_data = data[0]['fields']
-    album_artists = album_data['artists']
-    album_tracks = album_data['tracks']['items']
+    # Create a list of albums, each of which is a dictionary containing 
+    # the album data, artists, and tracks
+    albums = []
+    for album in data:
+        album_fields = album['fields']
+        album_artists = album_fields['artists']
+        album_tracks = album_fields['tracks']['items']
+        
+        album_info = {
+            'album_data': album_fields,
+            'album_artists': album_artists,
+            'album_tracks': album_tracks
+        }
+        
+        albums.append(album_info)
 
     # Pass the data to the template
     context = {
-        'album_data': album_data,
-        'album_artists': album_artists,
-        'album_tracks': album_tracks
+        'albums': albums
     }
 
     # Render the template with the data

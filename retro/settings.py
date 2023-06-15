8186cb7696e22bd0c491a6890d7bb4182f7f2fd0
Forgottenit <forgottenit@hotmail.com>
2023-06-15 14:31:59 +0000

    STYLING: Styled index.html and added nav-bar

--- retro/settings.py
+++ retro/settings.py
@@ -9,7 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+# colours 141529
+# 98d2eb 
+# f433ab
+# fbe689
+# f39237 
 from pathlib import Path
 
 import os

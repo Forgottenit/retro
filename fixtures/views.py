7118bf63399dd03b61777bbbf42474db3703a85a
Forgottenit <forgottenit@hotmail.com>
2023-06-18 20:53:11 +0000

    Formatted the album.html and fixed views in fixtures

--- fixtures/views.py
+++ fixtures/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
 import json
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from fixtures.get_fixtures import create_fixtures
 from django.urls import reverse
+from fixtures.get_fixtures import create_fixtures
+
+
+
+def format_duration(duration_ms):
+    minutes = duration_ms // 60000
+    seconds = (duration_ms // 1000) % 60
+
+    if minutes == 0:
+        return f"{seconds} secs"
+    else:
+        return f"{minutes}:{seconds:02d} mins"
 
 
 def album_view(request):
@@ -10,7 +21,7 @@ def album_view(request):
     with open('fixtures/filtered_albums/filtered_album_fixtures.json', 'r') as file:
         data = json.load(file)
 
-    # Create a list of albums, each of which is a dictionary containing 
+    # Create a list of albums, each of which is a dictionary containing
     # the album data, artists, and tracks
     albums = []
     for album in data:
@@ -18,11 +29,16 @@ def album_view(request):
         album_artists = album_fields['artists']
         album_tracks = album_fields['tracks']['items']
 
+        # Process album fields
         album_info = {
             'album_data': album_fields,
             'album_artists': album_artists,
-            'album_tracks': album_tracks
+            'album_tracks': album_tracks,
         }
+
+        # Process track details
+        for track in album_tracks:
+            track['duration_formatted'] = format_duration(track['duration_ms'])
 
         albums.append(album_info)
 
@@ -34,10 +50,9 @@ def album_view(request):
     # Render the template with the data
     return render(request, 'fixtures/album.html', context)
 
+
 def update_albums(request):
-    print("Request method:", request.method)  # This is for debugging.
     if request.method == 'POST':
         artist_name = request.POST.get('artist_name')
-        print("Artist name:", artist_name)  # This is for debugging.
         create_fixtures(artist_name)
     return HttpResponseRedirect(reverse('fixtures:album'))  # Redirect to the album page

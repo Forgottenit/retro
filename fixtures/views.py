82e66e0e441d7bb5e283af9260470074c06d1525
Forgottenit <forgottenit@hotmail.com>
2023-06-18 21:47:29 +0000

    Formatted Python code

--- fixtures/views.py
+++ fixtures/views.py
@@ -5,7 +5,6 @@ from django.urls import reverse
 from fixtures.get_fixtures import create_fixtures
 
 
-
 def format_duration(duration_ms):
     minutes = duration_ms // 60000
     seconds = (duration_ms // 1000) % 60
@@ -18,41 +17,45 @@ def format_duration(duration_ms):
 
 def album_view(request):
     # Load data from the JSON file
-    with open('fixtures/filtered_albums/filtered_album_fixtures.json', 'r') as file:
+    with open(
+        "fixtures/filtered_albums/filtered_album_fixtures.json",
+        "r",
+        encoding="utf-8",
+    ) as file:
         data = json.load(file)
 
     # Create a list of albums, each of which is a dictionary containing
     # the album data, artists, and tracks
     albums = []
     for album in data:
-        album_fields = album['fields']
-        album_artists = album_fields['artists']
-        album_tracks = album_fields['tracks']['items']
+        album_fields = album["fields"]
+        album_artists = album_fields["artists"]
+        album_tracks = album_fields["tracks"]["items"]
 
         # Process album fields
         album_info = {
-            'album_data': album_fields,
-            'album_artists': album_artists,
-            'album_tracks': album_tracks,
+            "album_data": album_fields,
+            "album_artists": album_artists,
+            "album_tracks": album_tracks,
         }
 
         # Process track details
         for track in album_tracks:
-            track['duration_formatted'] = format_duration(track['duration_ms'])
+            track["duration_formatted"] = format_duration(track["duration_ms"])
 
         albums.append(album_info)
 
     # Pass the data to the template
-    context = {
-        'albums': albums
-    }
+    context = {"albums": albums}
 
     # Render the template with the data
-    return render(request, 'fixtures/album.html', context)
+    return render(request, "fixtures/album.html", context)
 
 
 def update_albums(request):
-    if request.method == 'POST':
-        artist_name = request.POST.get('artist_name')
+    if request.method == "POST":
+        artist_name = request.POST.get("artist_name")
         create_fixtures(artist_name)
-    return HttpResponseRedirect(reverse('fixtures:album'))  # Redirect to the album page
+    return HttpResponseRedirect(
+        reverse("fixtures:album")
+    )  # Redirect to the album page
--- retro/settings.py
+++ retro/settings.py
@@ -10,106 +10,111 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 # colours 141529
-# 98d2eb 
+# 98d2eb
 # f433ab
 # fbe689
-# f39237 
+# f39237
 from pathlib import Path
 
 import os
 import dj_database_url
-if os.path.isfile('env.py'):
+
+if os.path.isfile("env.py"):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # Spotify Secret Key
 
-SPOTIFY_CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
+SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['forgottenit-retro.herokuapp.com', 'localhost', '.gitpod.io']
+ALLOWED_HOSTS = ["forgottenit-retro.herokuapp.com", "localhost", ".gitpod.io"]
 
 # Required CSRF_TRUSTED_ORIGINS for Django 4.2
-CSRF_TRUSTED_ORIGINS = ['https://forgottenit-retro.herokuapp.com', 'https://localhost', 'https://8000-forgottenit-retro-s9wz1pwll0t.ws-eu100.gitpod.io',]
+CSRF_TRUSTED_ORIGINS = [
+    "https://forgottenit-retro.herokuapp.com",
+    "https://localhost",
+    "https://8000-forgottenit-retro-s9wz1pwll0t.ws-eu100.gitpod.io",
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'cloudinary_storage',
-    'django.contrib.staticfiles',
-    'cloudinary',
-    'django.contrib.sites',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "cloudinary_storage",
+    "django.contrib.staticfiles",
+    "cloudinary",
+    "django.contrib.sites",
     "crispy_forms",
     "crispy_bootstrap5",
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.facebook',
-    'allauth.socialaccount.providers.google',
-    'home',
-    'fixtures',
-    'accounts',
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.facebook",
+    "allauth.socialaccount.providers.google",
+    "home",
+    "fixtures",
+    "accounts",
 ]
 
 SOCIALACCOUNT_PROVIDERS = {
-    'facebook': {
-        'APP': {
-            'client_id': 'your-facebook-app-client-id',
-            'secret': 'your-facebook-app-secret',
+    "facebook": {
+        "APP": {
+            "client_id": "your-facebook-app-client-id",
+            "secret": "your-facebook-app-secret",
         }
     },
-    'google': {
-        'APP': {
-            'client_id': 'your-google-app-client-id',
-            'secret': 'your-google-app-secret',
+    "google": {
+        "APP": {
+            "client_id": "your-google-app-client-id",
+            "secret": "your-google-app-secret",
         }
     },
-
 }
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'retro.urls'
+ROOT_URLCONF = "retro.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'allauth')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',  # required by allauth
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "templates", "allauth"),
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",  # required by allauth
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
@@ -119,37 +124,34 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 AUTHENTICATION_BACKENDS = [
-
     # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
+    "django.contrib.auth.backends.ModelBackend",
     # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
-
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
-LOGIN_URL = '/accounts/login'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = "/accounts/login"
+LOGIN_REDIRECT_URL = "/"
 
-WSGI_APPLICATION = 'retro.wsgi.application'
+WSGI_APPLICATION = "retro.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -162,16 +164,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -179,9 +181,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -191,16 +193,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "static/"
+STATICFILES_STORAGE = (
+    "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
+)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_URL = "/media/"
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"

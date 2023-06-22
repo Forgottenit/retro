bcbb8c0e891d12cdb61ddf134620152a7876e983
Forgottenit <forgottenit@hotmail.com>
2023-06-22 22:54:21 +0000

    changed product models

--- products/admin.py
+++ products/admin.py
@@ -1,48 +1,158 @@
 from django.contrib import admin
-from .models import Album, CD, Vinyl, TShirt
+from .models import Album, CD, Vinyl, TShirt, TShirtVariant
+from django.utils.safestring import mark_safe
 
 
-# Register your models here.
 @admin.register(Album)
 class AlbumAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "album_id",
+        "release_date",
+        "total_tracks",
+        "popularity",
+        "album_type",
+        "label",
+        "copyright",
+        "explicit",
+        "get_image",
+    )
+    search_fields = ("album_id", "label")
+
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(
+                '<img src="{url}" width="{width}" height={height} />'.format(
+                    url=obj.image.url,
+                    width=50,
+                    height=50,
+                )
+            )
+        else:
+            return "No Image"
+
+    get_image.short_description = "Image"
 
 
 @admin.register(CD)
 class CDAdmin(admin.ModelAdmin):
-    list_display = ("album", "price", "on_sale", "get_quantity")
+    list_display = (
+        "name",
+        "album",
+        "quantity",
+        "price",
+        "on_sale",
+        "get_image",
+    )
+    search_fields = ("name", "album__name")
     list_filter = ("on_sale",)
-    search_fields = ("album__album_id", "album__title")
 
-    def get_quantity(self, obj):
-        return obj.album.quantity
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(
+                '<img src="{url}" width="{width}" height={height} />'.format(
+                    url=obj.image.url,
+                    width=50,
+                    height=50,
+                )
+            )
+        else:
+            return "No Image"
 
-    get_quantity.short_description = "Quantity"
+    get_image.short_description = "Image"
 
 
 @admin.register(Vinyl)
 class VinylAdmin(admin.ModelAdmin):
-    list_display = ("album", "price", "on_sale", "get_quantity")
+    list_display = (
+        "name",
+        "album",
+        "quantity",
+        "price",
+        "on_sale",
+        "get_image",
+    )
+    search_fields = ("name", "album__name")
     list_filter = ("on_sale",)
-    search_fields = ("album__album_id", "album__title")
 
-    def get_quantity(self, obj):
-        return obj.album.quantity
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(
+                '<img src="{url}" width="{width}" height={height} />'.format(
+                    url=obj.image.url,
+                    width=50,
+                    height=50,
+                )
+            )
+        else:
+            return "No Image"
 
-    get_quantity.short_description = "Quantity"
+    get_image.short_description = "Image"
 
 
 @admin.register(TShirt)
 class TShirtAdmin(admin.ModelAdmin):
-    list_display = ("album", "price", "on_sale", "get_quantity", "get_sizes")
-    list_filter = ("on_sale",)
-    search_fields = ("album__album_id", "album__title")
+    list_display = (
+        "name",
+        "description",
+        "album",
+        "sleeve_length",
+        "colour",
+        "on_sale",
+        "get_image",
+    )
+    search_fields = ("name", "description", "album__album_id")
+    list_filter = ("on_sale", "sleeve_length", "colour")
 
-    def get_quantity(self, obj):
-        return obj.album.quantity
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(
+                '<img src="{url}" width="{width}" height={height} />'.format(
+                    url=obj.image.url,
+                    width=50,
+                    height=50,
+                )
+            )
+        else:
+            return "No Image"
 
-    def get_sizes(self, obj):
-        return ", ".join(obj.sizes.all().values_list("size", flat=True))
+    get_image.short_description = "Image"
 
-    get_quantity.short_description = "Quantity"
-    get_sizes.short_description = "Sizes"
+
+@admin.register(TShirtVariant)
+class TShirtVariantAdmin(admin.ModelAdmin):
+    list_display = (
+        "get_name",
+        "get_album",
+        "size",
+        "quantity",
+        "get_image",
+    )
+    search_fields = (
+        "tshirt__name",
+        "tshirt__album__album_id",
+        "size__size",
+    )
+
+    def get_name(self, obj):
+        return obj.tshirt.name
+
+    get_name.short_description = "T-Shirt Name"
+
+    def get_album(self, obj):
+        return obj.tshirt.album
+
+    get_album.short_description = "Associated Album"
+
+    def get_image(self, obj):
+        if obj.tshirt.image:
+            return mark_safe(
+                '<img src="{url}" width="{width}" height={height} />'.format(
+                    url=obj.tshirt.image.url,
+                    width=50,
+                    height=50,
+                )
+            )
+        else:
+            return "No Image"
+
+    get_image.short_description = "Image"

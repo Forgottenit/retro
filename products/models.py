01c652c5788e6e92d498ccda769a8add045e82a3
Forgottenit <forgottenit@hotmail.com>
2023-06-22 21:36:09 +0000

    Added more models to Products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -6,8 +7,15 @@ class Artist(models.Model):
     artist_id = models.CharField(
         max_length=100, unique=True, blank=True, null=True
     )
-    genres = models.TextField(blank=True, null=True)
+    genres = models.ManyToManyField("Genre")
     spotify_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -36,10 +44,15 @@ class Album(models.Model):
     copyright = models.CharField(max_length=100, blank=True, null=True)
     explicit = models.BooleanField(blank=True, null=True)
     artists = models.ManyToManyField(Artist)
+    genres = models.ManyToManyField(Genre)
     tracks = models.ManyToManyField(Track)
     quantity = models.IntegerField(blank=True, null=True)
     price = models.DecimalField(
-        max_digits=8, decimal_places=2, blank=True, null=True
+        max_digits=8,
+        decimal_places=2,
+        blank=True,
+        null=True,
+        validators=[MinValueValidator(0)],
     )
     on_sale = models.BooleanField(blank=True, null=True, default=False)
     spotify_url = models.URLField(blank=True, null=True)
@@ -47,3 +60,60 @@ class Album(models.Model):
 
     def __str__(self):
         return self.album_id
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        blank=True,
+        null=True,
+        validators=[MinValueValidator(0)],
+    )
+    on_sale = models.BooleanField(blank=True, null=True, default=False)
+    image = models.ImageField(
+        upload_to="product_images", blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class CD(Product):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"CD: {self.album}"
+
+
+class Vinyl(Product):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Vinyl: {self.album}"
+
+
+class TShirtSize(models.Model):
+    size = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.size
+
+
+class TShirt(Product):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    sizes = models.ManyToManyField(TShirtSize)
+
+    def __str__(self):
+        return f"T-Shirt: {self.album}"
+
+
+class TShirtQuantity(models.Model):
+    tshirt = models.ForeignKey(TShirt, on_delete=models.CASCADE)
+    size = models.ForeignKey(TShirtSize, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.tshirt} - {self.size}: {self.quantity}"
--- retro/settings.py
+++ retro/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     "home",
     "fixtures",
     "accounts",
+    "products",
 ]
 
 SOCIALACCOUNT_PROVIDERS = {

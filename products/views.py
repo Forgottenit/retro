c21df32773097c2285cf52330fa0b5f80b7d17fd
Forgottenit <forgottenit@hotmail.com>
2023-06-27 15:59:43 +0000

    TEMPLATES: Added template for Albums in products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Album
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+def album_model_view(request):
+    print("TEST")
+    albums = Album.objects.all().order_by(
+        "artists__name"
+    )  # Fetch album instances
+    paginator = Paginator(albums, 9)
+    page_number = request.GET.get("page")
+    albums = paginator.get_page(page_number)
+    return render(
+        request, "products/albums.html", {"albums": albums}
+    )  # Pass albums to template

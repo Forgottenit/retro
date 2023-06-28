0f72e63f5729853ace328702d2853229245c7f0a
Forgottenit <forgottenit@hotmail.com>
2023-06-28 12:46:44 +0000

    CHANGED AND TESTING BUG: Admin and models

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Album, CD, Vinyl, TShirt, TShirtVariant
+from django.db import models
+from .models import (
+    Album,
+    Artist,
+    Genre,
+    Track,
+    CD,
+    Vinyl,
+    TShirt,
+    TShirtVariant,
+    Image,
+)
 from django.utils.safestring import mark_safe
+
+
+class TrackInline(admin.TabularInline):
+    model = Track
+    extra = 1
+    classes = ("collapse",)
 
 
 class ImageDisplayMixin:
@@ -19,11 +36,55 @@ class ImageDisplayMixin:
     get_image.short_description = "Image"
 
 
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ("name", "album", "track_number", "duration", "explicit")
+    list_filter = ("name",)
+    search_fields = ("name", "album__album_id")
+    autocomplete_fields = ("album",)
+
+
+class AlbumArtist(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+
+class AlbumInline(admin.TabularInline):
+    model = AlbumArtist
+    extra = 1
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "artist_id",
+        "get_genres",
+        "get_albums",
+        "spotify_url",
+        "type",
+    )
+    list_filter = ("name",)
+    search_fields = ("name", "artist_id")
+    inlines = [AlbumInline]
+
+    def get_genres(self, obj):
+        return ", ".join([genre.name for genre in obj.genres.all()])
+
+    get_genres.short_description = "Genres"
+
+    def get_albums(self, obj):
+        return ", ".join([album.name for album in obj.albums.all()])
+
+    get_albums.short_description = "Albums"
+
+
 @admin.register(Album)
 class AlbumAdmin(ImageDisplayMixin, admin.ModelAdmin):
+    inlines = [TrackInline]
     list_display = (
+        "name",
         "display_artists",
-        "album",
         "artist_id",
         "album_id",
         "release_date",
@@ -38,7 +99,13 @@ class AlbumAdmin(ImageDisplayMixin, admin.ModelAdmin):
         "spotify_url",
         "get_image",
     )
-    search_fields = ("album_id", "label")
+    search_fields = (
+        "album_id",
+        "label",
+        "name",
+        "artist_id",
+        "artists__name",
+    )
 
     def display_artists(self, obj):
         return ", ".join([str(artist) for artist in obj.artists.all()])
@@ -47,7 +114,17 @@ class AlbumAdmin(ImageDisplayMixin, admin.ModelAdmin):
         return ", ".join([str(genre) for genre in obj.genres.all()])
 
     def display_tracks(self, obj):
-        return ", ".join([str(track) for track in obj.tracks.all()])
+        print("Tracks:", obj.track_set.all())
+        return ", ".join([str(track) for track in obj.track_set.all()])
+
+    def artist_name(self, obj):
+        return obj.artists.first().name if obj.artists.exists() else ""
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(artist_name=models.F("artists__name")).order_by(
+            "artist_name"
+        )
 
     display_artists.short_description = "Artists"
     display_genres.short_description = "Genres"

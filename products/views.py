2265f99126ebf03a1441b6283229ee690d24742c
Forgottenit <forgottenit@hotmail.com>
2023-07-01 23:54:04 +0000

    STYLING: Styled HTML for multiple Artists

--- products/views.py
+++ products/views.py
@@ -2,14 +2,17 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import messages
 from django.core.paginator import Paginator
-from django.db.models import Q
+from django.db.models import Q, Count
 from .models import Album, Genre
+from urllib.parse import urlencode
 
 
 def album_model_view(request):
     """A view to show all albums, including search queries"""
+    albums = Album.objects.annotate(artist_count=Count("artists")).order_by(
+        "artists__artist_name"
+    )
 
-    albums = Album.objects.all().order_by("artists__artist_name")
     search_query = request.GET.get("q")
 
     genres = Genre.objects.all()
@@ -17,7 +20,7 @@ def album_model_view(request):
     print(genre_query)
 
     if genre_query is not None and genre_query != "All":
-        albums = albums.filter(genres__name__icontains=genre_query).distinct()
+        albums = albums.filter(genres__name__icontains=genre_query)
 
     if search_query is not None:
         search_query = search_query.strip()
@@ -30,12 +33,33 @@ def album_model_view(request):
                 | Q(artists__artist_name__icontains=search_query)
             )
 
-    paginator = Paginator(albums, 21)
+    # Convert QuerySet to list and remove duplicates at Python level
+    albums = list(albums)
+    seen = set()
+    albums = [
+        x
+        for x in albums
+        if x.album_id not in seen and not seen.add(x.album_id)
+    ]
+
+    params = request.GET.copy()  # make a mutable copy of request.GET QueryDict
+    page_number = params.pop("page", None)  # remove 'page' if it exists
+    params_str = (
+        params.urlencode()
+    )  # convert the QueryDict to a URL-encoded string
+
+    paginator = Paginator(albums, 18)
     page_number = request.GET.get("page")
     albums = paginator.get_page(page_number)
 
     return render(
         request,
         "products/albums.html",
-        {"albums": albums, "search_term": search_query, "genres": genres},
+        {
+            "albums": albums,
+            "search_term": search_query,
+            "genres": genres,
+            "genre": genre_query,
+            "params": params_str,
+        },
     )

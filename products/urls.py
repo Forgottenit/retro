5a1dfa76adb37b030d6e6f8e87176e8950132a2f
Forgottenit <forgottenit@hotmail.com>
2023-07-03 17:22:40 +0000

    Added album detail view

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import album_model_view
+from .views import album_model_view, album_details
 
 app_name = "products"
 
 urlpatterns = [
     path("album/", album_model_view, name="albums"),
+    path("<str:album_id>", album_details, name="album_details"),
 ]

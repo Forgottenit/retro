9b950695b495c33db377506e7fdc8014bafb937f
Forgottenit <forgottenit@hotmail.com>
2023-07-04 19:14:54 +0000

    Added cart contents in contexts

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,27 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Album
 
 
 def cart_contents(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get("cart", {})
+    print(cart)
+
+    for item_id, quantity in cart.items():
+        album = get_object_or_404(Album, album_id=item_id)
+        total += quantity * album.price
+        product_count += quantity
+        cart_items.append(
+            {
+                "item_id": item_id,
+                "quantity": quantity,
+                "album": album,
+            }
+        )
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)

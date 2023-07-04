8b7cd20a02d02d31274702d67cb68ca9e9d84a7b
Forgottenit <forgottenit@hotmail.com>
2023-07-04 18:36:23 +0000

    ADDED add to cart with album_id

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import view_cart
+from .views import view_cart, add_to_cart
 
 app_name = "cart"
 
 urlpatterns = [
     path("", view_cart, name="cart"),
+    path("add/<item_id>/", add_to_cart, name="add_to_cart"),
 ]

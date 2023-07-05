525247d410f84c0ebd415dc4d4f630a9bcfc666b
Forgottenit <forgottenit@hotmail.com>
2023-07-05 15:55:34 +0000

    ADDED Edit And Delete for Cart and Styled

--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import view_cart, add_to_cart, empty_cart
+from . import views
 
 app_name = "cart"
 
 urlpatterns = [
-    path("", view_cart, name="cart"),
-    path("add/<item_id>/", add_to_cart, name="add_to_cart"),
-    path("empty/", empty_cart, name="empty_cart"),
+    path("", views.view_cart, name="view_cart"),
+    path("add/<item_id>/", views.add_to_cart, name="add_to_cart"),
+    path("empty/", views.empty_cart, name="empty_cart"),
+    path("edit/<item_id>/", views.edit_cart, name="edit_cart"),
+    path(
+        "delete/<item_id>/",
+        views.delete_from_cart,
+        name="delete_from_cart",
+    ),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_cart(request):
@@ -25,7 +25,46 @@ def add_to_cart(request, item_id):
 
 
 def empty_cart(request):
+    """Method to empty Cart when needed TO BE REMOVED"""
     request.session[
         "cart"
     ] = {}  # Set the cart dictionary to an empty dictionary
-    return redirect("cart:cart")
+    return redirect("cart:view_cart")
+
+
+def edit_cart(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    try:
+        quantity = int(request.POST.get("quantity"))
+    except ValueError:
+        return HttpResponse(
+            status=400
+        )  # Return 400 Bad Request error for invalid data
+
+    cart = request.session.get("cart", {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id, None)
+
+    request.session["cart"] = cart
+    return redirect(reverse("cart:view_cart"))
+
+
+def delete_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    try:
+        cart = request.session.get("cart", {})
+        cart.pop(item_id)
+        print("CART 1:", cart)
+        request.session["cart"] = cart
+        print("CART 2:", cart)
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(
+            status=500
+        )  # Return 500 Server Error for unexpected issues

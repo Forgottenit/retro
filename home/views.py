9b34b66cc073d42161645b75993c91238addcae9
Forgottenit <forgottenit@hotmail.com>
2023-07-12 20:19:00 +0000

    Added privacy policy

--- home/views.py
+++ home/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+
 # Create your views here.
 
 
 def index(request):
-    """ Returns Index page """
+    """Returns Index page"""
 
-    return render(request, 'home/index.html')
+    return render(request, "home/index.html")
+
+
+def privacy_policy(request):
+    with open(
+        "home/templates/home/privacy_policy/privacy_policy.txt", "r"
+    ) as file:
+        data = file.read().replace("\n", "<br/>")
+    return render(request, "home/privacy_policy.html", {"policy": data})

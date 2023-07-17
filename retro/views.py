5ca1a0bd9525654439428420c9091296eeeb0581
Forgottenit <forgottenit@hotmail.com>
2023-07-17 01:33:10 +0000

    Created error handler pages

--- retro/views.py
+++ retro/views.py
@@ -4,16 +4,16 @@ Module for error Handler views
 from django.shortcuts import render
 
 
-def handler_403(request, exception):
+def handler403(request, exception=None):
     """Error Handler 403"""
     return render(request, "error_handlers/403.html", status=403)
 
 
-def handler_404(request, exception):
+def handler404(request, exception=None):
     """Error Handler 404"""
     return render(request, "error_handlers/404.html", status=404)
 
 
-def handler_500(request, exception):
+def handler500(request, exception=None):
     """Error Handler 500"""
     return render(request, "error_handlers/500.html", status=500)

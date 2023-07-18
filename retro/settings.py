c0bd0109908abaa3dbd103b36455459c2172c189
Forgottenit <forgottenit@hotmail.com>
2023-07-18 10:59:52 +0000

    README

--- retro/settings.py
+++ retro/settings.py
@@ -184,17 +184,9 @@ WSGI_APPLICATION = "retro.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if os.environ.get("DEVELOPMENT") == "1" or os.environ.get("DEBUG") == "1":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        },
-    }
-else:
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
